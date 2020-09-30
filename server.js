cf2785898dbe334636c7c8807c5dab29554e64ed
James Frih <jamesfrih@me.com>
2020-09-30 13:48:59 +0200

    fixed utf8 hopefully

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 var app = express();
 
 var notes = ['shoppinglista', 'nyckelord', 'lösenord'];
-res.header("Content-Type", "application/json; charset=utf-8");
+
 
 app.get('/', (req,res) => {
+    res.header("Content-Type", "application/json; charset=utf-8");
     res.send('Tjena');
 });
 
 app.get('/:id', (req, res) => {
+    res.header("Content-Type", "application/json; charset=utf-8");
     if (req.params.id == 'notes') {
+        
         notes.forEach(function myFunction(item) {
         res.write(item + ' ');
         });
