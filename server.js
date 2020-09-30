526196552f8ed65ee31f6bafcc42fac40d33fec3
James Frih <jamesfrih@me.com>
2020-09-30 13:19:25 +0200

    added 404

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
+var notes = ['shoppinglista', 'nyckelord', 'lösenord'];
+
 app.get('/', (req,res) => {
     res.send('Tjena');
 });
 
-app.get('/notes')
+app.get('/notes', (req, res) => {
+    notes.forEach(element => res.send(element));
+});
+
+app.listen(8080);
+
+console.log('Server running...')
